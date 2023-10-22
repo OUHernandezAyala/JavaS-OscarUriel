80bf680807e29c067bbcb80c6a10de420ab5cc99
OUHernandezAyala <142889499+OUHernandezAyala@users.noreply.github.com>
2023-10-22 05:20:49 +0000

    Subiendo cambios para la optimización de la forma en como escribí mis arrays anteriormente.

--- src/app.js
+++ src/app.js
@@ -6,34 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let quien = new Array();
-  quien[0] = "Mi perro";
-  quien[1] = "Mi mama";
-  quien[2] = "Mi hermana";
+  let quien = ["Mi perro", "Mi mama", "Mi hermana"];
 
-  let accion = new Array();
-  accion[0] = "rompio";
-  accion[1] = "perdio";
-  accion[2] = "escondio";
+  let accion = ["rompio", "perdio", "escondio"];
 
-  let que = new Array();
-  que[0] = "mi tarea";
-  que[1] = "mis llaves";
-  que[2] = "mi mochila";
+  let que = ["mi tarea", "mis llaves", "mi mochila"];
 
-  let cuando = new Array();
-  cuando[0] = "ayer";
-  cuando[1] = "en la tarde";
-  cuando[2] = "hoy";
+  let cuando = ["ayer", "en la tarde", "hoy"];
 
-  {
-    document.querySelector("#parte1").innerHTML =
-      quien[Math.floor(Math.random() * quien.length)];
-    document.querySelector("#parte2").innerHTML =
-      accion[Math.floor(Math.random() * accion.length)];
-    document.querySelector("#parte3").innerHTML =
-      que[Math.floor(Math.random() * que.length)];
-    document.querySelector("#parte4").innerHTML =
-      cuando[Math.floor(Math.random() * cuando.length)];
-  }
+  document.querySelector("#parte1").innerHTML =
+    quien[Math.floor(Math.random() * quien.length)];
+  document.querySelector("#parte2").innerHTML =
+    accion[Math.floor(Math.random() * accion.length)];
+  document.querySelector("#parte3").innerHTML =
+    que[Math.floor(Math.random() * que.length)];
+  document.querySelector("#parte4").innerHTML =
+    cuando[Math.floor(Math.random() * cuando.length)];
 };
